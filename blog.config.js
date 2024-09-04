1d22f7b14821e4e1058a1b9ec618947c8a6b1169
WangRui1235 <129173205+WangRui1235@users.noreply.github.com>
2024-09-04 12:54:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -309,6 +309,12 @@ const BLOG = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
+      name: 'あの光',
+      artist: '乃木坂46',
+      url: 'https://music.163.com/song/media/outer/url?id=2609096382.mp3'
+      cover: 'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    },
+    {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
