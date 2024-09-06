d19047d32a45867bac32a597632b72cf81836fec
WangRui1235 <129173205+WangRui1235@users.noreply.github.com>
2024-09-06 23:30:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一位忙于学业的大三生', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://main--wrui.netlify.app/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://atrionline.me/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
